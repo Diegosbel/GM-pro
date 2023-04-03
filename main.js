854bcf360e97175a97ca71860c6f67191b66ab0d
Diego Sebastian Beltramo <Diegosbeltramo@gmail.com>
2023-04-02 23:39:29 -0300

    agregado de funciones superiores y class

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
 
+// FUNCIONES
+
 const resultado = (producto) => {
     alert(producto + ESPACIO + "añadido al carro de compras")
     console.log(producto);
 }
 
+
+
+
+// INICIO DE PAGINA Y COMUNICACION AL CLIENTE
 
 alert("Bienvenido a GMpro!\nA continuacion ingresa tus datos para empezar")
 
@@ -25,6 +31,10 @@ alert("Hola" + ESPACIO + nombre + ESPACIO + apellido + ESPACIO + "ya validamos t
 
 alert("Estos son nuestros productos")
 
+
+
+// DESCRIPCION DE PRODUCTOS
+
 const producto = ['PROCESADOR', 'PLACA MADRE', 'MEMORIA RAM', 'DISCO SOLIDO', 'FUENTE', 'GABINETE']
 
 for (let index = 0; index < producto.length; index++) {
@@ -32,62 +42,169 @@ for (let index = 0; index < producto.length; index++) {
 
 }
 
-console.log("Productos añadidos:")
-
-
-let rta = " "
-
-
-do {
-
-
-    let producto = prompt("Ingrese el producto que desea añadir al carro de compras : \nProcesador $25,000 \nPlaca madre $18.000 \nMemoria ram $11.000 \nDisco solido $15.000\nFuente $9.000\nGabinete $19.000").toUpperCase()
+// CLASES Y FUNCIONES DE ORDEN SUPERIOR
 
 
 
-    switch (producto) {
-        case "PROCESADOR":
-            resultado("Procesador")
-            alert("$25000 añadido a la cuenta")
-            break;
 
-        case "PLACA MADRE":
-            resultado("Placa madre")
-            alert("$18000 añadido a la cuenta")
-            break;
+class Producto {
+    constructor
+        (id, nombre, precio) {
 
-        case "MEMORIA RAM":
-            resultado("Memoria ram")
-            alert("$11000 añadido a la cuenta")
-            break;
 
-        case "DISCO SOLIDO":
-            resultado("Disco solido")
-            alert("$15000 añadido a la cuenta")
-            break;
+        this.id = id
+        this.nombre = nombre
+        this.precio = precio
+        this.cantidad = 1
+        this.stock = 10
+    }
+}
 
-        case "FUENTE":
-            resultado("Fuente")
-            alert("$9000 añadido a la cuenta")
-            break;
+// Lista de procesadores
 
-        case "GABINETE":
-            resultado("Gabinete")
-            alert("$19000 añadido a la cuenta")
-            break;
-
-        default:
-            alert("Usted no ingreso un producto valido")
-            break;
+class Procesador {
+    constructor() {
+        this.listaProcesadores = []
     }
 
-    rta = prompt("Desea añadir un nuevo producto?\nDe lo contrario escriba ESC para salir").toUpperCase()
 
-} while (rta != "ESC") {
+    procesadores() {
+        this.listaProcesadores =
+            [new Producto(1, "Ryzen 3", 23000),
+            new Producto(2, "Ryzen 5", 37000),
+            new Producto(3, "Ryzen 7", 53000),
+            new Producto(4, "Intel I5", 36000),
+            new Producto(5, "Intel I7", 55000),
+            new Producto(6, "Intel I9", 72000)]
+    }
 
-alert("Lo estamos reedireccionando al carro de compras")
+
+    verProcesador() {
+        let lista = ""
+        this.listaProcesadores.forEach(producto => {
+            lista += "\nid =" + " " + producto.id + ")" + " " + producto.nombre + " " + "$" + producto.precio
+        })
+        return lista
+    }
+
+
+    agregarProcesador(id) {
+
+        return this.listaProcesadores.find(el => el.id == id)
+    }
 
 }
+
+// LISTA PLACA MADRE
+
+class Mother {
+    constructor() {
+        this.listaPlacas = []
+    }
+
+
+    placasMadres() {
+        this.listaPlacas =
+            [new Producto(1, "Mother MSI A320M-A PRO AM4", 25400),
+            new Producto(2, "Mother Gigabyte GA-A320M-H AM4", 24000),
+            new Producto(3, "Mother ASUS TUF B450M-PLUS II AM4", 45900),
+            new Producto(4, "Mother ASUS ROG STRIX X570-E WIFI II", 157700),]
+    }
+
+
+    verPlacas() {
+        let lista = ""
+        this.listaPlacas.forEach(producto => {
+            lista += "\nid =" + " " + producto.id + ")" + " " + producto.nombre + " " + "$" + producto.precio
+        })
+        return lista
+    }
+
+
+    agregarPlaca(id) {
+
+        return this.listaPlacas.find(el => el.id == id)
+    }
+}
+
+// Carro de compras
+class carroCompras {
+    constructor() {
+        this.listaProductos = []
+    }
+
+    itemAgregado(producto) {
+
+        this.listaProductos.push(producto)
+
+
+    }
+
+    compraTotal() {
+
+        let lista = 0
+
+        this.listaProductos.forEach(producto => {
+            lista += producto.precio * producto.cantidad
+        })
+
+        return lista;
+
+
+    }
+
+
+
+}
+
+
+// OBJETOS CONTROLADORES
+
+const procesadorAgregado = new Procesador()
+const total = new carroCompras()
+const placaMadreAgregado = new Mother()
+procesadorAgregado.procesadores()
+placaMadreAgregado.placasMadres()
+
+
+
+let rta = ""
+
+
+
+// PROCESO DE AÑADIR COMPRAS
+
+do {
+    
+// PROCESADORES
+
+    alert("Seleccione el procesador que desea añadir" + procesadorAgregado.verProcesador());
+    
+    let id = prompt("Ingrese el id de procesador a agregar")
+    
+    const productos = procesadorAgregado.agregarProcesador(id)
+    if (productos) {
+        total.itemAgregado(productos)
+    } else { alert("Procesador no añadido") }
+    
+    
+
+// PLACAS MADRES
+    
+    alert("Seleccione la mother que desea añadir" + placaMadreAgregado.verPlacas());
+
+    id = prompt("Ingrese el id de la mother a agregar")
+
+    const placa = placaMadreAgregado.agregarPlaca(id)
+    if (placa) {
+        total.itemAgregado(placa)
+    } else { alert("mother no añadido") }
+
+    rta = prompt("Ingrese 'finalizar' para terminar su compra.\nSi desea agregar mas productos ingrese 'continuar' para agregar mas productos").toUpperCase()
+
+} while (rta != "FINALIZAR")
+
+
 
 
 
@@ -95,10 +212,7 @@ let formaPago = " "
 
 while (formaPago != "FINALIZAR") {
 
-    let tipoPago = prompt("¿Que tipo de pago desea realizar? Efectivo obtiene un 10% de descuento en su compra\nEfectivo\nDebito\nTarjeta de Credito").toUpperCase()
-
-    let total = Number(prompt("Ingrese su total"))
-
+    let tipoPago = prompt("Su total es de $" + total.compraTotal() + "\n¿Que tipo de pago desea realizar? Efectivo obtiene un 10% de descuento en su compra\nEfectivo\nDebito\nTarjeta de Credito").toUpperCase()
 
 
     if (tipoPago == "EFECTIVO") {
@@ -106,16 +220,16 @@ while (formaPago != "FINALIZAR") {
         alert("Pago con efectivo seleccionado usted obtiene un 10% de descuento")
 
 
-        let descuento = total * 0.10
+        let descuento = total.compraTotal() * 0.10
 
-        let precioFinal = total - descuento
+        let precioFinal = total.compraTotal() - descuento
 
         alert("Su nuevo total es de : $" + precioFinal)
 
     } else if (tipoPago == "DEBITO") {
 
         alert("Pago con debito seleccionado")
-        alert("Su nuevo total es de: $" + total)
+        alert("Su nuevo total es de: $" + total.compraTotal())
 
     } else if (tipoPago == "TARJETA DE CREDITO") {
 
@@ -125,16 +239,16 @@ while (formaPago != "FINALIZAR") {
 
         switch (cuota) {
             case 1:
-                alert("Su nuevo total es de: $" + total)
+                alert("Su nuevo total es de: $" + total.compraTotal())
                 break;
             case 3:
-                alert("Su nuevo total es de: $" + parseInt(total * 1.05))
+                alert("Su nuevo total es de: $" + parseInt(total.compraTotal() * 1.05)+ "\nPago por cuota $" + parseInt(total.compraTotal() * 1.05 / 12))
                 break;
             case 6:
-                alert("Su nuevo total es de: $" + parseInt(total * 1.10))
+                alert("Su nuevo total es de: $" + parseInt(total.compraTotal() * 1.10)+ "\nPago por cuota $" + parseInt(total.compraTotal() * 1.10 / 12))
                 break;
             case 12:
-                alert("Su nuevo total es de: $" + parseInt(total * 1.15))
+                alert("Su nuevo total es de: $" + parseInt(total.compraTotal() * 1.15)+ "\nPago por cuota $" + parseInt(total.compraTotal() * 1.15 / 12))
                 break;
             default:
                 alert("Ingrese un valor de cuota correcto")
@@ -150,6 +264,4 @@ while (formaPago != "FINALIZAR") {
 }
 
 alert("Gracias por su compra!")
-
-
 
