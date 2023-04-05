ec79924543f31122e45e5b0b6950b7bb1089e6c7
Diego Sebastian Beltramo <Diegosbeltramo@gmail.com>
2023-04-04 23:08:38 -0300

    segunda pre-entrega

--- main.js
+++ main.js
@@ -35,7 +35,7 @@ alert("Estos son nuestros productos")
 
 // DESCRIPCION DE PRODUCTOS
 
-const producto = ['PROCESADOR', 'PLACA MADRE', 'MEMORIA RAM', 'DISCO SOLIDO', 'FUENTE', 'GABINETE']
+const producto = ['PROCESADOR', 'PLACA MADRE', 'MEMORIA RAM']
 
 for (let index = 0; index < producto.length; index++) {
     alert(producto[index]);
@@ -44,39 +44,46 @@ for (let index = 0; index < producto.length; index++) {
 
 // CLASES Y FUNCIONES DE ORDEN SUPERIOR
 
-
+alert("Ingresa los productos que quisieras agregar a la compra")
 
 
 class Producto {
-    constructor
-        (id, nombre, precio) {
-
-
-        this.id = id
-        this.nombre = nombre
-        this.precio = precio
-        this.cantidad = 1
-        this.stock = 10
+    constructor(id, nombre, precio) {
+        
+        this.id = id;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.cantidad = 1;
     }
 }
 
-// Lista de procesadores
 
-class Procesador {
+// AÑADIDO DE LISTA DE LOS PRODUCTOS
+
+
+class lista {
     constructor() {
-        this.listaProcesadores = []
+        this.listaProcesadores = [];
+        this.listaMother = [];
+        this.ram = [];
     }
+
+
+
+    // LISTA DE PROCESADORES
 
 
     procesadores() {
         this.listaProcesadores =
-            [new Producto(1, "Ryzen 3", 23000),
-            new Producto(2, "Ryzen 5", 37000),
-            new Producto(3, "Ryzen 7", 53000),
-            new Producto(4, "Intel I5", 36000),
-            new Producto(5, "Intel I7", 55000),
-            new Producto(6, "Intel I9", 72000)]
+            [new Producto(1, "AMD Ryzen 3 4100", 32450),
+            new Producto(2, "AMD Ryzen 5 3600 4.2GHz Turbo AM4", 54990),
+            new Producto(3, "AMD Ryzen 7 7700 5.3GHz Turbo AM5", 160400),
+            new Producto(4, "Intel Core i3 10100F 4.3GHz", 28950),
+            new Producto(5, "Intel Core i5 10400F 4.3GHz", 62000),
+            new Producto(6, "Intel Core i7 10700F 4.8GHz", 113990)]
     }
+
+
 
 
     verProcesador() {
@@ -88,23 +95,21 @@ class Procesador {
     }
 
 
-    agregarProcesador(id) {
+
+    anadirProcesador(id) {
 
         return this.listaProcesadores.find(el => el.id == id)
     }
 
-}
-
-// LISTA PLACA MADRE
-
-class Mother {
-    constructor() {
-        this.listaPlacas = []
-    }
 
 
-    placasMadres() {
-        this.listaPlacas =
+
+    // LISTA DE MOTHER
+
+
+
+    mother() {
+        this.listaMother =
             [new Producto(1, "Mother MSI A320M-A PRO AM4", 25400),
             new Producto(2, "Mother Gigabyte GA-A320M-H AM4", 24000),
             new Producto(3, "Mother ASUS TUF B450M-PLUS II AM4", 45900),
@@ -112,22 +117,60 @@ class Mother {
     }
 
 
-    verPlacas() {
+
+
+    verMother() {
         let lista = ""
-        this.listaPlacas.forEach(producto => {
+        this.listaMother.forEach(producto => {
             lista += "\nid =" + " " + producto.id + ")" + " " + producto.nombre + " " + "$" + producto.precio
         })
         return lista
     }
 
 
-    agregarPlaca(id) {
+    anadirMother(id) {
 
-        return this.listaPlacas.find(el => el.id == id)
+        return this.listaMother.find(el => el.id == id)
     }
+
+
+
+    // LISTA DE MEMORIA RAM
+
+
+    memoriaRam() {
+        this.listaRam =
+        [new Producto(1, "Memoria Crucial DDR4 4GB 2666MHz Value", 10230),
+        new Producto(2, "Memoria Team DDR4 4GB 2400MHz Elite Plus Red", 11650),
+        new Producto(3, "Memoria Kingston DDR4 8GB 3200MHz Fury Beast CL16", 17430),
+        new Producto(4, "Memoria Team DDR4 8GB 2666MHz T-Force Vulcan Z Gray", 18550),
+        new Producto(5, "Memoria Team DDR4 8GB 3200MHz Elite Red", 18800),
+        new Producto(6, "Memoria Patriot Viper DDR4 8GB 3200MHz Steel RGB", 20150)]
+    }
+
+
+    verRam() {
+        let lista = ""
+        this.listaRam.forEach(producto => {
+            lista += "\nid =" + " " + producto.id + ")" + " " + producto.nombre + " " + "$" + producto.precio
+        })
+        return lista
+    }
+
+
+    anadirRam(id) {
+
+        return this.listaRam.find(el => el.id == id)
+    }
+
+
 }
 
-// Carro de compras
+
+
+// CARRITO DE COMPRAS
+
+
 class carroCompras {
     constructor() {
         this.listaProductos = []
@@ -158,61 +201,73 @@ class carroCompras {
 }
 
 
-// OBJETOS CONTROLADORES
+// INSTANCIAR CLASES
 
-const procesadorAgregado = new Procesador()
+const agregarProducto = new lista()
 const total = new carroCompras()
-const placaMadreAgregado = new Mother()
-procesadorAgregado.procesadores()
-placaMadreAgregado.placasMadres()
 
+agregarProducto.procesadores()
+agregarProducto.mother()
+agregarProducto.memoriaRam()
+total.compraTotal()
 
 
 let rta = ""
 
-
-
-// PROCESO DE AÑADIR COMPRAS
-
 do {
-    
-// PROCESADORES
 
-    alert("Seleccione el procesador que desea añadir" + procesadorAgregado.verProcesador());
-    
+    // PROCESADORES
+
+    alert("Seleccione el procesador que desea añadir" + agregarProducto.verProcesador());
+
     let id = prompt("Ingrese el id de procesador a agregar")
-    
-    const productos = procesadorAgregado.agregarProcesador(id)
+
+    const productos = agregarProducto.anadirProcesador(id)
     if (productos) {
         total.itemAgregado(productos)
     } else { alert("Procesador no añadido") }
-    
-    
 
-// PLACAS MADRES
-    
-    alert("Seleccione la mother que desea añadir" + placaMadreAgregado.verPlacas());
+
+
+    // PLACAS MADRES
+
+    alert("Seleccione la mother que desea añadir" + agregarProducto.verMother());
 
     id = prompt("Ingrese el id de la mother a agregar")
 
-    const placa = placaMadreAgregado.agregarPlaca(id)
+    const placa = agregarProducto.anadirMother(id)
     if (placa) {
         total.itemAgregado(placa)
     } else { alert("mother no añadido") }
+
+
+
+
+
+    // MEMORIA RAM
+
+
+    alert("Seleccione la memoria ram que desea añadir" + agregarProducto.verRam());
+
+    id = prompt("Ingrese el id de la memoria ram a agregar")
+
+    const ram = agregarProducto.anadirRam(id)
+    if (ram) {
+        total.itemAgregado(ram)
+    } else { alert("memoria ram no añadido") }
 
     rta = prompt("Ingrese 'finalizar' para terminar su compra.\nSi desea agregar mas productos ingrese 'continuar' para agregar mas productos").toUpperCase()
 
 } while (rta != "FINALIZAR")
 
 
-
-
+// INSTANCIA DE PAGO
 
 let formaPago = " "
 
 while (formaPago != "FINALIZAR") {
 
-    let tipoPago = prompt("Su total es de $" + total.compraTotal() + "\n¿Que tipo de pago desea realizar? Efectivo obtiene un 10% de descuento en su compra\nEfectivo\nDebito\nTarjeta de Credito").toUpperCase()
+    let tipoPago = prompt("Su total es de $" + total.compraTotal() + "\n¿Que tipo de pago desea realizar? Efectivo obtiene un 10% de descuento en su compra\nEfectivo\nDebito\nCredito").toUpperCase()
 
 
     if (tipoPago == "EFECTIVO") {
@@ -231,7 +286,7 @@ while (formaPago != "FINALIZAR") {
         alert("Pago con debito seleccionado")
         alert("Su nuevo total es de: $" + total.compraTotal())
 
-    } else if (tipoPago == "TARJETA DE CREDITO") {
+    } else if (tipoPago == "CREDITO") {
 
         ("Pago con credito seleccionado")
 
